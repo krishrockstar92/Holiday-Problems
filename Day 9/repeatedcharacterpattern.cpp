4c092e540aec14cea9dd9ab852b498f3696bc64d
krishrockstar92 <krrish9183106@gmail.com>
2026-06-10 19:08:38 +0530

    Day 9

---
+++ Day 9/repeatedcharacterpattern.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number of rows: ";
+    cin>>n;
+    for(int i=65; i<65+n; i++){
+        for(int j=65; j<=i; j++){
+            cout<<char(i);
+        }
+        cout<<endl;
+    }
+    return 0;
+}
